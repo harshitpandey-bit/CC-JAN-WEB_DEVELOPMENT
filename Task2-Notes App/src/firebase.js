ad0807ae6a304c4458fc439bbe51e6c359eb1a5a
Laser7235 <harshitpandeymrj007@gmail.com>
2023-02-01 00:55:33 +0530

    done

--- Task2-Notes App/src/firebase.js
+++ Task2-Notes App/src/firebase.js
@@ -3,14 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCvA4UCts5hutLcdw4h5PgtoVHUCr4ZHq8",
+  authDomain: "todo-app-d41d2.firebaseapp.com",
+  projectId: "todo-app-d41d2",
+  storageBucket: "todo-app-d41d2.appspot.com",
+  messagingSenderId: "995910260619",
+  appId: "1:995910260619:web:d6e4cbacd6ddc8eda3018e"
 };
 
 const app = initializeApp(firebaseConfig);
